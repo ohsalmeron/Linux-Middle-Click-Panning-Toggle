ed7e4e92cc06e4c8aaccfa41aa1f10fa87b084b4
Omar Hernandez Salmeron <ohsalmeron@gmail.com>
2023-12-06 12:47:41 -0600

    Update toggle.sh

--- toggle.sh
+++ toggle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Detect the mouse device with "pointer" capability
-DEVICE_NAME=$(xinput list --name-only | grep -i 'pointer')
+# Set the device name for your mouse (replace YourDeviceName)
+DEVICE_NAME="YourDeviceName"
 
 # Check if the script is called with an argument
 if [ $# -eq 0 ]; then
@@ -11,17 +11,14 @@ fi
 
 # Enable or disable middle-click panning based on the argument
 if [ "$1" = "on" ]; then
-  # Enable middle-click panning
   xinput set-prop "$DEVICE_NAME" "libinput Scroll Method Enabled" 0, 0, 1
   xinput set-prop "$DEVICE_NAME" "libinput Button Scrolling Button" 2
   echo "Middle-click panning is now enabled for $DEVICE_NAME."
 elif [ "$1" = "off" ]; then
-  # Disable middle-click panning
   xinput set-prop "$DEVICE_NAME" "libinput Scroll Method Enabled" 0, 0, 0
   xinput set-prop "$DEVICE_NAME" "libinput Button Scrolling Button" 0
   echo "Middle-click panning is now disabled for $DEVICE_NAME."
 else
-  # Display usage instructions if an invalid argument is provided
   echo "Usage: $0 [on|off]"
   exit 1
 fi
